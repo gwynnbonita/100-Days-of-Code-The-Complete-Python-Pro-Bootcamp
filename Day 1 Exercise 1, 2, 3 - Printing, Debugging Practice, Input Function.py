b7fff63ff74d2c6d6a88f1eaac618531a37de582
Hera Bonita <116147053+gwynnbonita@users.noreply.github.com>
2023-04-08 11:33:47 +0800

    Add files via upload

---
+++ Day 1 Exercise 1, 2, 3 - Printing, Debugging Practice, Input Function.py
@@ -0,0 +1,20 @@
+# Exercise 1 - Printing
+# Program that prints and use the Python print function.
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# Exercise 2 - Debugging Practice
+# Program that the code is fixed and runs without errors.
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# Exercise 3 - Input Function
+# Program that prints the number of characters in a user's name.
+
+print(len(input("What is your name? ")))
+# len() - a function that calculates the lenght of a string.
